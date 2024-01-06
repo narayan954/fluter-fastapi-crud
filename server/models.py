04634b1f5308d11ed96103fb9551d298d4353277
Narayan soni <narayansoni854@gmail.com>
2024-01-06 17:57:55 +0530

    fixed deprecated parameters

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ import uuid
 from typing import Optional
 from pydantic import BaseModel, Field
 
+
 class Book(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
     title: str = Field(...)
@@ -9,13 +10,13 @@ class Book(BaseModel):
     synopsis: str = Field(...)
 
     class Config:
-        allow_population_by_field_name = True
-        schema_extra = {
+        populate_by_name = True
+        json_schema_extra = {
             "example": {
                 "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
                 "title": "Don Quixote",
                 "author": "Miguel de Cervantes",
-                "synopsis": "..."
+                "synopsis": "...",
             }
         }
 
@@ -26,10 +27,10 @@ class BookUpdate(BaseModel):
     synopsis: Optional[str]
 
     class Config:
-        schema_extra = {
+        json_schema_extra = {
             "example": {
                 "title": "Don Quixote",
                 "author": "Miguel de Cervantes",
-                "synopsis": "Don Quixote is a Spanish novel by Miguel de Cervantes..."
+                "synopsis": "Don Quixote is a Spanish novel by Miguel de Cervantes...",
             }
         }
