ed82ea23b4499d47012a7e4565e4645bebdfd089
Narayan soni <narayansoni854@gmail.com>
2024-01-06 19:37:27 +0530

    update fastapi application to utilise opencv for image processing

--- server/main.py
+++ server/main.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI
-from dotenv import dotenv_values
-from pymongo import MongoClient
-from routes import router as book_router
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-
-config = dotenv_values(".env")
+from pydantic import BaseModel
+import cv2
+import numpy as np
+import requests
+from io import BytesIO
 
 app = FastAPI()
 
+# Allow CORS for all origins
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,22 +18,46 @@ app.add_middleware(
 )
 
 
-@app.on_event("startup")
-def startup_db_client():
-    app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
-    print("Connected to MongoDB")
+class ImageRequest(BaseModel):
+    image_url: str
 
 
-@app.on_event("shutdown")
-def shutdown_db_client():
-    app.mongodb_client.close()
-    print("Disconnected from MongoDB")
+def process_image_opencv(image_url: str) -> str:
+    try:
+        # Fetch the image from the URL
+        response = requests.get(image_url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+
+        # Read the image using OpenCV
+        image = cv2.imdecode(
+            np.frombuffer(image_data.read(), np.uint8), cv2.IMREAD_COLOR
+        )
+
+        # Our image processing code with OpenCV goes here
+        # For example, convert the image to grayscale
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # display the grayscale image
+        cv2.imshow("Grayscale image", gray_image)
+        cv2.waitKey()
+
+        # We can save the processed image or return some information
+        # Here, we'll just return a success message
+        return "Image processed successfully"
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=f"Error processing image: {str(e)}")
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def home():
+    return {"message": "Hello World"}
 
 
-app.include_router(book_router, tags=["books"], prefix="/book")
+@app.post("/process_image")
+def process_image(image_request: ImageRequest):
+    print("image req", image_request.image_url)
+    result = process_image_opencv(image_request.image_url)
+    return {"message": result}
